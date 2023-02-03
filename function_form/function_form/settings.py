5e7a57711e1ba51fe55cdfb952dced8ca5700a49
Hariharan0007 <=>
2023-02-03 16:50:06 +0530

    time and date format specified

--- function_form/function_form/settings.py
+++ function_form/function_form/settings.py
@@ -115,6 +115,14 @@ USE_I18N = True
 USE_TZ = True
 
 
+DATE_INPUT_FORMAT = [
+    '%d/%m/%Y',
+]
+
+TIME_INPUT_FORMAT = [
+    '%H:%M',
+]
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
